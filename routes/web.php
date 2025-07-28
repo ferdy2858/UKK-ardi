0fe29202d7b2424cc60a392c0b4876f0b0f19537
ferdy2858 <ggferdy88@gmail.com>
2025-07-28 21:47:59 +0700

    mbalen bolo

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
-use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\TranksaksiController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::resource('siswa', SiswaController::class);
-Route::resource('transaksi', TranksaksiController::class);
+
+require __DIR__.'/auth.php';
