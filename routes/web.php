73169aa411f627a5ba659ffe28c841217228f4a6
ferdy2858 <ggferdy88@gmail.com>
2025-07-28 19:21:44 +0700

    manuk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
